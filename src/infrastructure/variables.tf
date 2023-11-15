c0e79307113119392e6ddb05dcb82428c7a8f4ed
Karamkb2000 <karamkb15@gmail.com>
2023-11-15 23:31:32 +0300

    Implement config

---
+++ src/infrastructure/variables.tf
@@ -0,0 +1,33 @@
+variable "region" {
+  type = string
+  default = "eu-central-1"
+}
+
+variable "config" {
+  type    = string
+  default = "~/.aws/config"
+}
+
+variable "credentials" {
+  type    = string
+  default = "~/.aws/credentials"
+}
+
+
+variable "my_ip" {
+  description = "ip address"
+  type        = string
+  default = "176.92.42.121"
+}
+
+variable "zone_one" {
+  description = "region"
+  type        = string
+  default = "eu-central-1a"
+}
+
+variable "zone_two" {
+  description = "region"
+  type        = string
+  default = "eu-central-1b"
+}
